7feb59dfe5a266fbb83b04af625af12405d9be6c
Ossian Borén <ossianboren@gmail.com>
2018-10-15 08:46:53 +0200

    first commit

---
+++ Assets/Scripts/PodController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor;
+    public bool steering;
+}
+
+public class PodController : MonoBehaviour
+{
+    public List<AxleInfo> axleInfos;
+    public float maxMotorTorque;
+    public float maxSteeringAngle;
+    public GameObject arrow;
+    Quaternion direction;
+    float directionAngle;
+    Quaternion rotation;
+    float steering;
+    float thrust;
+    float turnValue;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        thrust = 0f;
+        directionAngle = 0f;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // finds the corresponding visual wheel
+    // correctly applies the transform
+    public void ApplyLocalPositionToVisuals(WheelCollider collider)
+    {
+        if (collider.transform.childCount == 0)
+        {
+            return;
+        }
+
+        Transform visualWheel = collider.transform.GetChild(0);
+
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
+
+        visualWheel.transform.position = position;
+        visualWheel.transform.rotation = rotation;
+    }
+
+
+    public void FixedUpdate()
+    {
+        directionAngle += Input.GetAxis("Horizontal");
+        float rotationAngle = transform.rotation.y;
+        steering = Mathf.Clamp(0, rotationAngle, directionAngle);
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+            ApplyLocalPositionToVisuals(axleInfo.leftWheel);
+            ApplyLocalPositionToVisuals(axleInfo.rightWheel);
+        }
+    }
+}
---
+++ Assets/Scripts/RotateAround.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAround : MonoBehaviour
+{
+    public GameObject origo;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = origo.transform.position + Vector3.forward * 5;
+        transform.Translate(Vector3.right * Input.GetAxis("Horizontal"));
+
+        Vector3 directionToOrigo = origo.transform.position - transform.position;
+        transform.rotation = Quaternion.FromToRotation(Vector3.back, directionToOrigo);
+
+    }
+}
